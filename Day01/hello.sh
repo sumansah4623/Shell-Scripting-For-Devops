1509c06050299aed5b4435bf6962df3fc582d809
suman <sumansah4623@gmail.com>
2025-08-20 10:18:15 +0000

    My first shell scripting file

---
+++ Day01/hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# this is script for suman 
+
+echo "suman to everyone hello dosto"
+
+echo "i just newly created youtube channel on nature so all of you who is intersed can suscribe my channal"
+
+
