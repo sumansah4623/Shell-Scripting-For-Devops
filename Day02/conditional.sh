f2e8a29e3ce8925b74f9258d731639af561181cf
suman <sumansah4623@gmail.com>
2025-08-20 17:10:30 +0000

    more else condition added

--- Day02/conditional.sh
+++ Day02/conditional.sh
@@ -5,10 +5,14 @@ this programm is example of conditional statement
 comment
 
 read -p "Enter the name of girl : " girl
+read -p "love percentage % : " love
 
 if [[ $girl == "sita" ]];
 then	
 	echo "this is that girl"
+elif [[ $love -ge 100 ]];
+then 
+        echo "this is true love for sita"	
 else
         echo "this is not that girl"	
 fi
