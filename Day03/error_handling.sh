882be253b008c517583d408c972f3a1a7b150318
suman <sumansah4623@gmail.com>
2025-08-21 05:43:47 +0000

    error handle shell scripting program

---
+++ Day03/error_handling.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# this is function
+create_directory() {
+	mkdir demo
+}
+
+
+# call function here
+create_directory
+
+# display on screen
+echo "this should not work because the code is interrupted"
